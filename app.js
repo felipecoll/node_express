268556ea5080db327049629b91aa60a527e5e587
Felipe Coll <leonfelipecoll@gmail.com>
2025-05-30 22:08:51 -0300

    Instalacion de dotEnv mediante npm install dotenv

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+console.log(PORT)
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');  
+    res.send(`
+        <h1>Curso express Js</h1>
+        <p>Este es un ejemplo de una aplicación Express.</p>
+        <p>Corre en el puerto ${PORT} de momento...</p>
+        `);  
 });
 
 app.listen(PORT, () => {
